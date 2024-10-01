df4bd5f811a0ea4ea42071086f7dd223ec4d4cba
rahullkiran <rahulkiran698@gmail.com>
2024-10-01 18:30:27 -0400

    Navbar
    Has icons plus logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import logo from "../assets/kevinRushLogo.png";
+import {FaGit, FaLinkedin} from "react-icons/fa"; 
+import {FaGithub} from "react-icons/fa"; 
+import { SiGmail } from "react-icons/si";
+
 const navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <img src={logo} alt="logo" />
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin />
+            <FaGithub />
+            <SiGmail />
+        </div>
+    </nav>
   )
 }
 
