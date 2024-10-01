b3038375adb0b46abf7c3e2c638ec49968571b0e
rahullkiran <rahulkiran698@gmail.com>
2024-10-01 17:57:01 -0400

    App.jsx fix
    displays properly now

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
-//this needs work as well
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">hello world!</h1>
+    </div>
+  )
+}
+
+export default App
