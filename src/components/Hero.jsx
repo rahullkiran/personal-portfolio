5dc0b7d305e133d132b2d954539c09dfd12f2494
rahullkiran <rahulkiran698@gmail.com>
2024-10-02 20:05:59 -0400

    picture is up
    hero page now has picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import FancyText from '@carefully-coded/react-text-gradient';
+import { HERO_CONTENT } from '../constants';
+import pfp from "../assets/kevinRushProfile.jpg"; 
 const hero = () => {
   return (
     <div className ="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -17,7 +18,14 @@ const hero = () => {
                     <span className="bg-clip-text text-4xl font-thin tracking-tight ">
                         Computer Engineering @ York University 
                     </span>
-                    
+                    <p className="my-2 max-w-xl py-6 font-thin tracking-tighter">
+                        {HERO_CONTENT}
+                    </p>
+                </div>
+            </div>
+            <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex justify-center">
+                    <img src={pfp} alt="Rahul Kiran" />
                 </div>
             </div>
         </div>
