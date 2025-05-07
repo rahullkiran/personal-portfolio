35727a7a3eeeb06a00ee30ea8f3487417c3eb3c2
rahullkiran <rahulkiran698@gmail.com>
2025-05-06 21:54:39 -0400

    Cleaned up folders
    removed some bloat

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,3 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
-
 export const HERO_CONTENT = `Computer Engineering @ York University`;
 
 export const ABOUT_TEXT = [
@@ -31,37 +26,6 @@ export const EXPERIENCES = [
       'Designed front-end UI and implemented back-end functionality based on key design principles',
       'Led team meetings to present progress reports and projections, while coordinating workload distribution through effective collaboration ']
 
-  },
-];
-
-export const PROJECTS = [
-  {
-    title: "E-Commerce Website",
-    image: project1,
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
   },
 ];
 
