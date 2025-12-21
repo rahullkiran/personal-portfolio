c4643bd2772d41a2654ce5a30170913d8b231014
rahullkiran <rahulkiran698@gmail.com>
2025-12-21 13:40:28 +0530

    Updated picture
    Small update to pic in "About" section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import aboutImg from "../assets/rahulpic.jpg";
+import aboutImg from "../assets/rahulpic.jpeg";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 
